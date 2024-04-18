3e90e98d9d32cab5733d23c99a67a5a35be627cd
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-04-18 15:39:10 +0200

    add more test endpoints

--- backend/controllers/mongooseController.js
+++ backend/controllers/mongooseController.js
@@ -1,3 +1,25 @@
 exports.getTestMongoose = (req, res) => {
-  res.send('Här testar vi Mongoose.');
+  res.send('GET Mongoose.');
+};
+
+exports.getTestMongooseParam = (req, res) => {
+  const { params } = req.params;
+  res.send('GET Mongoose params: ' + params);
+};
+
+exports.postTestMongooseBody = (req, res) => {
+  const { body } = req.body;
+  res.send('POST Mongoose body: ' + JSON.stringify(body));
+};
+
+exports.postTestMongoose = (req, res) => {
+  res.send('POST Mongoose.');
+};
+
+exports.deleteTestMongoose = (req, res) => {
+  res.send('DELETE Mongoose.');
+};
+
+exports.putTestMongoose = (req, res) => {
+  res.send('PUT Mongoose.');
 };
