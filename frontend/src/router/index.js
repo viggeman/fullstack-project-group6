6e4f2c46c50ad0edb78919a581d687aa6e14d0cf
Christopher <christopher.rydberg@iths.se>
2024-04-25 11:42:42 +0200

    skapade Vue och uppdaterade index med vue-fil

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import FetchWriter from '../view/FetchWriter.vue';
+import FetchActor from '../view/FetchActor.vue';
 
-const routes = [{ path: '/fetch-writer', component: FetchWriter }];
+const routes = [
+    { path: '/fetch-writer', component: FetchWriter },
+    { path: '/fetch-actor', component: FetchActor },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
