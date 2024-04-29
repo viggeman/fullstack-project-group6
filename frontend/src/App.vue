5220b067a0f1988f6a870fc5dae18a002f702c33
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-04-29 11:49:14 +0200

    add menu and root page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import HelloWorld from './components/HelloWorld.vue';
+  import Menu from './components/Menu.vue';
 </script>
 
 <template>
   <!-- <HelloWorld msg="Hello + GRuPp 6" />
    -->
+  <Menu />
   <div class="container">
     <router-view />
   </div>
---
+++ frontend/src/view/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the home page of your application.</p>
+  </div>
+</template>
+
+<script setup>
+  // Add your component logic here
+</script>
+
+<style scoped>
+  /* Add your component styles here */
+</style>
