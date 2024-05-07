652512984bc081ff14c35a61039f66604b108886
Christopher <christopher.rydberg@iths.se>
2024-05-07 09:30:13 +0200

    menu

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,71 +1,78 @@
 <script setup>
-  import { useUserStore } from '../stores/userStore';
-  import { ref, onMounted, watch } from 'vue';
+import { useUserStore } from '../stores/userStore';
+import { ref, onMounted, watch } from 'vue';
 
-  const userStore = useUserStore();
-  const isLoggedIn = ref(userStore.isLoggedIn);
+const userStore = useUserStore();
+const isLoggedIn = ref(userStore.isLoggedIn);
 
-  userStore.$subscribe(() => {
+userStore.$subscribe(() => {
     isLoggedIn.value = userStore.isLoggedIn;
-  });
+});
 
-  const handleLogout = () => {
+const handleLogout = () => {
     userStore.logout();
-  };
+};
 </script>
 
 <style>
-  .header {
+.header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-  }
+}
 
-  .main-nav {
+.main-nav {
     display: flex;
     align-items: center;
     height: 7rem;
     margin-left: 2rem;
-  }
+}
 
-  .login-nav {
+.login-nav {
     display: flex;
     align-items: center;
     height: 7rem;
     margin-right: 2rem;
     color: black;
-  }
+}
 
-  .menu-col1 {
+.menu-col1 {
     display: flex;
     list-style: none;
     margin-left: 4rem;
-  }
+}
 
-  .menu-col1 li {
+.menu-col1 li {
     margin-right: 4rem;
-  }
+}
 
-  .menu-col1 li:last-child {
+.menu-col1 li:last-child {
     margin-right: 0;
-  }
+}
 </style>
 
 <template>
-  <header class="header">
-    <nav class="main-nav">
-      <router-link to="/"><h2>GRUPP6</h2></router-link>
-      <ul class="menu-col1">
-        <li><router-link to="/fetch-movie-actor">MovieActors</router-link></li>
-        <li><router-link to="/fetch-writer">Writers</router-link></li>
-        <li><router-link to="/fetch-actor">Actors</router-link></li>
-        <li><router-link to="/fetch-genre">Genres</router-link></li>
-        <li><router-link to="/users">Users</router-link></li>
-      </ul>
-    </nav>
-    <nav class="login-nav">
-      <router-link v-if="!isLoggedIn" to="/user-login">Log In</router-link>
-      <button v-else @click="handleLogout">Logout</button>
-    </nav>
-  </header>
+    <header class="header">
+        <nav class="main-nav">
+            <router-link to="/"><h2>GRUPP6</h2></router-link>
+            <ul class="menu-col1">
+                <li><router-link to="/fetch-movie">Movie</router-link></li>
+                <li>
+                    <router-link to="/fetch-movie-actor"
+                        >MovieActors</router-link
+                    >
+                </li>
+                <li><router-link to="/fetch-writer">Writers</router-link></li>
+                <li><router-link to="/fetch-actor">Actors</router-link></li>
+                <li><router-link to="/fetch-genre">Genres</router-link></li>
+                <li><router-link to="/users">Users</router-link></li>
+            </ul>
+        </nav>
+        <nav class="login-nav">
+            <router-link v-if="!isLoggedIn" to="/user-login"
+                >Log In</router-link
+            >
+            <button v-else @click="handleLogout">Logout</button>
+        </nav>
+    </header>
 </template>
